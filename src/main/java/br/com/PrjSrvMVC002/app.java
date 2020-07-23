8c7701abf81b9336578e1ab7a6daed15957ef41d
Marcos Ferreira <mrsilva8@debian9.stefanini.com>
2020-07-22 21:41:43 -0300

    Meu primeiro commit PrjSrvMVC002

---
+++ src/main/java/br/com/PrjSrvMVC002/app.java
@@ -0,0 +1,18 @@
+package br.com.PrjSrvMVC002;
+
+/**
+ * 
+ * @author mrsilva8
+ *
+ */
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/**
+ * Mensagem na console
+ */
+		System.out.println("Hello world!");
+	}
+
+}
